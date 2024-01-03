c18dcde2a44d321f82883a2d838a377a382be393
ShonChassel <gershoni1234@gmail.com>
2024-01-03 18:43:23 +0200

    first commit

--- components/shared/Dropdown.tsx
+++ components/shared/Dropdown.tsx
@@ -67,7 +67,7 @@ const Dropdown = ({ value, onChangeHandler }: DropdownProps) => {
             </AlertDialogHeader>
             <AlertDialogFooter>
               <AlertDialogCancel>Cancel</AlertDialogCancel>
-              <AlertDialogAction onClick={() => startTransition(handleAddCategory)}>Add</AlertDialogAction>
+              <AlertDialogAction >Add</AlertDialogAction>
             </AlertDialogFooter>
           </AlertDialogContent>
         </AlertDialog>
